16d25e81a514d8fe741c30704338dc061b42c8ea
lizhifu <fflizhifu@chinatelecom.cn>
2026-05-18 14:57:46 +0800

    init: 项目初始化

---
+++ src/main/java/com/example/smartconsult/SmartConsultApplication.java
@@ -0,0 +1,12 @@
+package com.example.smartconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartConsultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartConsultApplication.class, args);
+    }
+}
